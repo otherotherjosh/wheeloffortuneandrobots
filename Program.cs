2653083efbecb1b16636fb6e26e5b95f4e844d38
Josh Finnie <itsotherjosh@gmail.com>
2023-07-20 10:48:49 +1200

    Init classes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Player player3 = new Player();
+            Player player4 = new Player();
         }
+    }
+
+    /// <summary>
+    /// has all the shit where we get a random word
+    /// from a long list of words or phrases
+    /// and categories
+    /// </summary>
+    public class WordGame
+    {
+
+    }
+
+    /// <summary>
+    /// where players
+    /// </summary>
+    public class Player
+    {
+        public struct PlayerBase
+        {
+            public string Name;
+            public string ;
+        }
+    }
+
+
+    public class AI: Player
+    {
+
     }
 }
