cd70a44516815ede5b066467176dfd4690b1b9f3
Josh Finnie <itsotherjosh@gmail.com>
2023-07-21 15:53:39 +1200

    Reinitialized

--- Player.cs
+++ Player.cs
@@ -9,27 +9,10 @@ public class Player
     public int score;
     public string color;
     public double money = 0;
-
-    // deal with this shit later
-    public char GuessLetter()
-    {
-        return 'A';
-    }
-
-    public void DisplayTurn()
-    {
-        Decor.TextColor(color);
-        Console.WriteLine($"{name}'s turn:\n{money:C}");
-    }
 }
 
 
 public class AI : Player
 {
-    //public char GuessLetter()
-    //{
-    //    Random random = new Random();
-    //    int letterShift = random.Next(26);
-    //    return (char)('A' + letterShift);
-    //}
+    
 }
